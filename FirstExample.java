1e6dff9c060eb0c55734aa3cb6d420d90b55d71e
Love Gruvberg <love.gruvberg@gmail.com>
2021-02-16 13:33:11 +0100

    Created the main method

---
+++ FirstExample.java
@@ -0,0 +1,21 @@
+public class FirstExample {
+    public static void main(String[] args){
+
+    }
+}
+
+/* 
+Git - useful commands (locally):
+git init
+git status
+git add .
+git commit
+git log
+
+Git - useful commands (remotely):
+git remote add origin #LINK TO YOUR REPOSITORY# (first time)
+git push -u origin master (first time)
+git push
+git pull
+git fetch
+*/
